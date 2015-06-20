fe8c0306630d316a890b103bc2568d4f8be20d46
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 20:59:30 -0300

    versao 1.0

---
+++ src/engenhariaa/A.java
@@ -0,0 +1,31 @@
+
+package engenhariaa;
+
+public class A {
+
+    private int A1;
+
+    private float A2;
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    public int getA() {
+        return A1;
+    }
+
+    public void setA(int A) {
+        this.A1 = A1;
+    }
+
+ 
+    public  void main(String[] args) {
+        
+    }
+    
+}
