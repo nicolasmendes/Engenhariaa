53dbed8c3c4553edff0f945c2c7d9ab2ff1db51d
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 21:22:44 -0300

    versao 1.0

--- src/engenhariaa/A.java
+++ src/engenhariaa/A.java
@@ -6,6 +6,17 @@ public class A {
     private int A1;
 
     private float A2;
+    
+    private String A3;
+
+    public String getA3() {
+        return A3;
+    }
+
+    public void setA3(String A3) {
+        this.A3 = A3;
+    }
+
 
     public float getA2() {
         return A2;
@@ -15,11 +26,11 @@ public class A {
         this.A2 = A2;
     }
 
-    public int getA() {
+    public int getA1() {
         return A1;
     }
 
-    public void setA(int A) {
+    public void setA1(int A1) {
         this.A1 = A1;
     }
 
